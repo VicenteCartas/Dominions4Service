76e33b516c249ab9bf62d68d6491f23965fa4a5b
Vicente Cartas Espinel <vicente.cartas@outlook.com>
2016-04-24 13:17:31 -0700

    Client service start
    Initial work on the client service to detect turns

---
+++ Utils/PathUtils.cs
@@ -0,0 +1,33 @@
+﻿namespace Utils
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // http://stackoverflow.com/questions/1633028/calculating-the-path-relative-to-some-root-the-inverse-of-path-combine
+    // Used without permission
+    public class PathUtils
+    {
+        static public string NormalizeFilepath(string filepath)
+        {
+            string result = System.IO.Path.GetFullPath(filepath).ToLowerInvariant();
+
+            result = result.TrimEnd(new[] { '\\' });
+
+            return result;
+        }
+
+        public static string GetRelativePath(string rootPath, string fullPath)
+        {
+            rootPath = NormalizeFilepath(rootPath);
+            fullPath = NormalizeFilepath(fullPath);
+
+            if (!fullPath.StartsWith(rootPath))
+                throw new Exception("Could not find rootPath in fullPath when calculating relative path.");
+
+            return "." + fullPath.Substring(rootPath.Length);
+        }
+    }
+}
